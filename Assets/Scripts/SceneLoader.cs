18924f10a1b97cc542a2d3c14378e3ecad40961a
Ted Dong <tdong@bluewt365.com>
2018-09-30 13:10:54 -0400

    resetting files

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public float timeToMenu;
+    
+
+    private void Update()
+    {
+        timeToMenu -= Time.deltaTime;
+
+        if (timeToMenu < 0f)
+        {timeToMenu -= Time.deltaTime;
+        
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
